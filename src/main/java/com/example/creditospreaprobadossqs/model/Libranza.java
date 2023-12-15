db4aec7578c4833d2e0a2ebaebd1309ba7a9c3ad
ljbricen <ljbricen@bancolombia.com.co>
2023-12-14 21:39:22 -0500

    Se agrega formato ejemplo de fecha de vencimiento

--- src/main/java/com/example/creditospreaprobadossqs/model/Libranza.java
+++ src/main/java/com/example/creditospreaprobadossqs/model/Libranza.java
@@ -1,9 +1,12 @@
 package com.example.creditospreaprobadossqs.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record Libranza(
     String prestamo,
     String consumido,
     String documento,
+    @Schema(description = "Fecha de vencimiento", example = "2023-12-31")
     String vencimiento
 ) {
 }
